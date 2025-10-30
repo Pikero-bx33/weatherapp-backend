23669ae383d41dd09ce1f948ed21061fc68d63c4
guillaume legros <legrosguillaume@yahoo.fr>
2025-10-30 17:25:29 +0100

    projet weather app4

---
+++ urls.js
@@ -0,0 +1,7 @@
+// Insert the backend URL given by Vercel in this variable
+const BACKEND_URL = 'https://weatherapp-backend-two-gamma.vercel.app/';
+
+// Insert the frontend URL given by Vercel in this variable
+const FRONTEND_URL = 'https://weatherapp-frontend-lilac.vercel.app/';
+
+module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
